5686a0fa867e111f5450b33bba3470021b8955f7
Cesar William <cesarwbr@gmail.com>
2019-02-28 07:21:43 -0300

    improve the console menu

--- index.go
+++ index.go
@@ -4,8 +4,10 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
+// Content store the search preferences
 type Content struct {
 	searchTerm string
 	prefix     string
@@ -15,26 +17,31 @@ func main() {
 	var searchTerm = askAndReturnSearchTerm()
 	var prefix = askAndReturnPrefix()
 
-	content := Content{searchTerm, prefix}
-
-	fmt.Println(content)
+	if prefix != "Cancel" {
+		content := Content{searchTerm, prefix}
+		fmt.Printf("\n{searchTerm: %s, prefix: %s}", content.searchTerm, content.prefix)
+	}
 }
 
 func askAndReturnSearchTerm() string {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("Type a Wikipedia search term:")
+	fmt.Printf("Type a Wikipedia search term: ")
 	text, _ := reader.ReadString('\n')
+
+	text = strings.TrimSuffix(text, "\n")
 
 	return text
 }
 
 func askAndReturnPrefix() string {
+	fmt.Printf("\n")
 	var prefixes = [3]string{"Who is", "What is", "The history of"}
 	var prefix int
 	fmt.Println("[1] Who is")
 	fmt.Println("[2] What is")
 	fmt.Println("[3] The history of")
-	fmt.Println("[0] Cancel")
+	fmt.Printf("[0] Cancel\n\n")
+	fmt.Printf("Which type: ")
 	fmt.Scanf("%d", &prefix)
 
 	if (prefix > 3) || (prefix < 1) {
